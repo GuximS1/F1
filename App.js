f5ef3d3d87496fa29b455a5aee3840ddd6838168
Guxim <guxim.selmani1@hotmail.com>
2021-07-11 02:31:42 +0200

    F1 Reaction.

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View, Image, Text } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}> F1 Reaction</Text>
+      <View style={styles.big}>
+        <Image source={require('./assets/imageedit_11_9865605316.png')} style={styles.img} />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  title: {
+    position: 'absolute',
+    flex: 1,
+    fontSize: 50,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    paddingBottom: 300,
+    paddingRight: 10,
+    color: 'red',
+    textShadowColor: 'black',
+    textShadowOffset: { width: -2, height: 2 },
+    textShadowRadius: 10
+  },
+  img: {
+    resizeMode: 'center'
+
+  }
+})
